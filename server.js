50a9fd06efad0219214c6832e9cd8dc38250d59c
Logan Huston <shadows7590@gmail.com>
2023-10-21 13:28:39 -0600

    change port stuff

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const cors = require('cors');
 const app = express();
-const port = 3000;
+const port = process.emv.PORT || 3000;
 const userDataPath = 'users.json';
 
 app.use(cors());
