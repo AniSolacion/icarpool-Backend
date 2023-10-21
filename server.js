85386c9fb9ac5c8e52b082fa000caf89c351586f
Logan Huston <shadows7590@gmail.com>
2023-10-21 14:09:53 -0600

    Added reservations add new, get all API's

--- server.js
+++ server.js
@@ -11,11 +11,14 @@ app.use(cors());
 
 app.use(express.json());
 
+// User API's
+// Get all users
 app.get('/users', (req, res) => {
     const data = JSON.parse(fs.readFileSync(userDataPath, 'utf8'));
     res.json(data.users);
 });
 
+// Add new user
 app.post('/users', (req, res) => {
     const data = JSON.parse(fs.readFileSync(userDataPath, 'utf8'));
     const newUser = req.body;
@@ -25,11 +28,14 @@ app.post('/users', (req, res) => {
     res.json(newUser);
 });
 
+// Post APi's
+// Get all posts
 app.get('/posts', (req, res) => {
     const data = JSON.parse(fs.readFileSync(postDataPath, 'utf8'));
     res.json(data.posts);
 });
 
+// Add new post
 app.post('/posts', (req, res) => {
     const data = JSON.parse(fs.readFileSync(postDataPath, 'utf8'));
     const newPost = req.body; 
@@ -38,21 +44,24 @@ app.post('/posts', (req, res) => {
     fs.writeFileSync(postDataPath, JSON.stringify(data, null, 2), 'utf8');
     res.json(newPost);
 });
-/*
+
+// Reservation API's
+// Get all reservations
 app.get('/reservations', (req, res) => {
-    const data = JSON.parse(fs.readFileSync(userDataPath, 'utf8'));
+    const data = JSON.parse(fs.readFileSync(reservationDataPath, 'utf8'));
     res.json(data.reservations);
 });
 
+// Add new reservation
 app.post('/reservations', (req, res) => {
-    const data = JSON.parse(fs.readFileSync(userDataPath, 'utf8'));
-    const newPost = req.body; /// Change this??
-    newPost.id = data.posts.length + 1;
-    data.posts.push(newPost);
-    fs.writeFileSync(userDataPath, JSON.stringify(data, null, 2), 'utf8');
+    const data = JSON.parse(fs.readFileSync(reservationDataPath, 'utf8'));
+    const newReservation = req.body; 
+    newReservation.id = data.reservations.length + 1;
+    data.reservations.push(newReservation);
+    fs.writeFileSync(reservationDataPath, JSON.stringify(data, null, 2), 'utf8');
     res.json(newReservation);
 });
-*/
+
 app.listen(port, () => {
     console.log(`Server is running on ${port}`);
 });
